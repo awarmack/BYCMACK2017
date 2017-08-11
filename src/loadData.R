c7e0c253c7af6ba6acbda31da467f85debd7afb5
Adam Warmack <adam.warmack@gmail.com>
2017-08-11 16:25:02 -0400

    adding data by minute

--- src/loadData.R
+++ src/loadData.R
@@ -6,7 +6,7 @@ options(scipen=999)
 #Summarise by second, 5 seconds
 rm(list=ls())
 
-load("./alldat.RDATA")
+load("./dat/alldat.RDATA")
 
 
 #summarise by minute
@@ -16,6 +16,12 @@ alldat$TIME.S <- as.POSIXct(round(alldat$TIME.S, "secs"))
 
 #get performance data
 
+#summarise by minute
+
+alldat$TIME.M <- as.POSIXct(round(alldat$TIME.S, "mins"))
+alldat$TIME.S <- as.POSIXct(round(alldat$TIME.S, "secs"))
+
+#summarise Performance Data by Minute
 perf <- alldat %>% group_by(TIME.M) %>% summarise(SOG=mean(GPS.SOG, na.rm = TRUE), 
                                                   AWS=mean(AWS, na.rm = TRUE), 
                                                   AWA=mean(AWA, na.rm = TRUE), 
@@ -23,25 +29,11 @@ perf <- alldat %>% group_by(TIME.M) %>% summarise(SOG=mean(GPS.SOG, na.rm = TRUE
                                                   LAT=mean(GGA.LAT, na.rm=TRUE),
                                                   LON=mean(GGA.LON, na.rm=TRUE))
 
+#remove any over 180 and convert 
 perf$AWA[perf$AWA > 180] <- 360 - perf$AWA[perf$AWA > 180] 
 perf <- perf[perf$AWA > 0, ] 
 
-
-#break AWS into bins
-perf$AWS.range <- cut(perf$AWS, breaks=c(0, 6, 8, 10, 12, 16, 20, 24, 35))
-#perf$AWS.range <- gsub("]", ")", perf$AWS.range)
-#perf$AWS.range <- gsub(",", "~", perf$AWS.range)
-
-#perf$AWS.range <- ordered(perf$AWS.range, levels=c("(0~6)", "(6~8)", "(8~10)", "(10~12)", "(12~16)", "(16~20)", "(20~24)", "(24~35)"))
-
-#break AWA into bins 
-perf$AWA.range <- cut(perf$AWA, breaks=seq(0,180, by=15))
-
-
-
-#convert AWS, AWA, and SOG to TWS and TWD
-
-
+#convert Apparent to True
 getTrueWind <- function(AWS, AWA, SOG) { 
   #outputs a list with 
   # $tws = True Wind Speed
@@ -49,67 +41,51 @@ getTrueWind <- function(AWS, AWA, SOG) {
   
   b <- AWA * (pi/180)
   
+  #true wind speed
   TWS <- sqrt(AWS^2 + SOG^2 - 2*AWS*SOG*cos(b))
   
-  # from polars: 
-  #VTW = SQRT( (VAW * SIN(BAW))^2 + (VAW x cos(BAW)-Vboat)^2)
-  #
-  # VTW = velocity true wind
-  # VAW = Velocity Apparent
-  # BAW = Bearing of Apparent Wind
-  #
   
-  #TWS <- sqrt( (AWS * sin(b))^2 + (AWS * cos(b)-SOG)^2)
-  
-  
-  
-  #BTW = arc tan ( (VAW x sin(BAW)) / (VAW x cos(BAW)-vboat))
-  
-  
-  TWA <- atan( (AWS * sin(b))/ (AWS * cos(b)-SOG))
-  TWA <- TWA*180/pi
+  calcTWA <- function(b, AWS.b, SOG.b, W){
+    if(b > 0 & b<2*pi) {
+      
+      TWA <- acos(((AWS.b * cos(b)) - SOG.b)/W)
+      
+    } else {
+      if(b > 2*pi) {
+        TWA <- NA
+      }
+      
+      TWA <- -acos(AWS.b * cos(b) - SOG.b)
+      
+    }
     
+    if(!is.na(TWA)){
+      
+      TWA <- TWA*180/pi
+      
+    }
+    return(TWA)
+  }
   
-  #calcTWA <- function(b, AWS.b, SOG.b, W){
-  #   if(b > 0 & b<2*pi) {
-  #     
-  #     TWA <- acos(((AWS.b * cos(b)) - SOG.b)/W)
-  #     
-  #   } else {
-  #     if(b > 2*pi) {
-  #       TWA <- NA
-  #     }
-  #     
-  #     TWA <- -acos(AWS.b * cos(b) - SOG.b)
-  #     
-  #   }
-  #   
-  #   if(!is.na(TWA)){
-  #     
-  #     TWA <- TWA*180/pi
-  #     
-  #   }
-  #   return(TWA)
-  # }
+  TWA <- mapply(FUN=calcTWA, b, AWS, SOG, TWS)
   
-  #TWA <- mapply(FUN=calcTWA, b, AWS, SOG, TWS)
-
   x <- list(TWS=TWS, TWA=TWA)
   
-  
-  }
+}
 
 tw <- getTrueWind(perf$AWS, perf$AWA, perf$SOG)
 perf$TWS <- tw$TWS
 perf$TWA <- tw$TWA
 
 
-#add point of sail to perf
+#break wind speed into bins: 
+perf$AWS.range <- cut(perf$AWS, breaks=c(0, 6, 8, 10, 12, 16, 20, 24, 35))
+perf$AWS.range <- factor(perf$AWS.range, levels=rev(levels(perf$AWS.range)))
+perf$TWS.range <- cut(perf$TWS, breaks=c(0, 6, 8, 10, 12, 16, 20, 24, 35))
+perf$TWS.range <- factor(perf$TWS.range, levels=rev(levels(perf$TWS.range)))
 
+#break wind angle into bins:
+perf$AWA.range <- cut(perf$AWA, breaks=seq(0,180, by=30))
+perf$TWA.range <- cut(perf$AWA, breaks=seq(0,180, by=30))
 
-waypoints <- alldat %>% filter(!is.na(BWC.WPT)) %>% group_by(BWC.WPT) %>% summarise(LAT = mean(WPT.LAT),
-                                                                                  LON = mean(WPT.LON))
-
-
-
-
+write.csv(perf, "./dat/data_byminute.csv", row.names = FALSE)
--- src/perfAnalysis.R
+++ src/perfAnalysis.R
@@ -1,119 +1,90 @@
 #Performance analysis vs. Polars
+library(dplyr)
+library(tidyr)
 
 source("./src/loadData.R")
 source("./src/loadPolars.R")
 
 
 
-library(akima)
-library(ggplot2)
+options(scipen=999)
+
+getwd()
+#Summarise by second, 5 seconds
+rm(list=ls())
 
 
 
-# get the polar targets
-targets <- interpp(x=pol$BAW, 
-                      y=pol$VAW, 
-                      z=pol$V, 
-                      xo = perf$AWA, 
-                      yo= perf$AWS,
-                      linear=FALSE,
-                      extrap=FALSE,
-                      duplicate = "mean")
+#summarise by minute
 
-#Calculate Measurables
-perf$target.SOG <- targets$z
-perf$diff.SOG <- perf$SOG - perf$target.SOG
-perf$pol.perc <- (perf$SOG / perf$target.SOG) * 100
+alldat$TIME.M <- as.POSIXct(round(alldat$TIME.S, "mins"))
+alldat$TIME.S <- as.POSIXct(round(alldat$TIME.S, "secs"))
 
+#summarise Performance Data by Minute
+perf <- alldat %>% group_by(TIME.M) %>% summarise(SOG=mean(GPS.SOG, na.rm = TRUE), 
+                                                  AWS=mean(AWS, na.rm = TRUE), 
+                                                  AWA=mean(AWA, na.rm = TRUE), 
+                                                  COG=mean(GPS.COG, na.rm=TRUE), 
+                                                  LAT=mean(GGA.LAT, na.rm=TRUE),
+                                                  LON=mean(GGA.LON, na.rm=TRUE))
 
-#plot the performance
-ggplot() + 
-  geom_point(data=perf,  aes(x=AWA, y=SOG, color=AWS), alpha=0.7) + 
-  coord_polar("x") + 
-  scale_x_continuous(limits=c(0,360), breaks=seq(0,360, 15))+
-  scale_y_continuous(limits=c(0,8), breaks=seq(0,8, by=2))+
-  scale_color_gradientn(colors=rainbow(5), breaks=seq(0,30, 5))+
-  ggtitle("Speed Over Ground [SOG] by Apparent Wind Angle and Wind Speed")
+#remove any over 180 and convert 
+perf$AWA[perf$AWA > 180] <- 360 - perf$AWA[perf$AWA > 180] 
+perf <- perf[perf$AWA > 0, ] 
 
-ggsave("./figs/01_SOG_by AWA and AWS.png", width=10, height=10, units="cm", scale=2)
-
-
-
-
-#histogram of Apparent Wind Angle
-ggplot(perf, aes(AWA, fill=AWS.range)) + 
-  geom_histogram(binwidth = 15, position = "stack") + 
-  coord_polar() + 
-  scale_x_continuous(limits=c(0,360), breaks=seq(0,360,15), name = "Apparent Wind Angle [AWA]")+
-  scale_color_discrete(name="AWS Range [kts]")+
-  ggtitle("Distribution of Apparent Wind & Direction ")
-
-ggsave("./figs/02_Distribution Apparent Wind and Direction.png", width=10, height=10, units="cm", scale=2)
-
-
-
-#plot the path
-library(ggmap)
-map_lims <- c(min(perf$LON), min(perf$LAT), max(perf$LON), max(perf$LAT)) 
-
-lkhuron <- get_map(location = map_lims, zoom=8)
-
-ggmap(lkhuron) + 
-  geom_path(data=perf, aes(x=LON, y=LAT, color=SOG), size=2) +
-  scale_color_gradientn(colors=rainbow(5), limits=c(0,10), breaks=seq(0,10, 2))
-
-ggmap(lkhuron) + 
-  geom_path(data=perf, aes(x=LON, y=LAT, color=pol.perc), size=2)+
-  scale_color_gradient2(midpoint=100, mid="blue", name="% of Polar")
-
-
-#Speed over time
-
-t_lims <- c(as.POSIXct("2017-07-22 20:00:00"), as.POSIXct("2017-07-24 04:00:00"))
-
-ggplot(perf, aes(x=TIME.M, y=SOG, color=SOG)) + geom_path() +
-  ggtitle("GPS Speed Over Ground")
-
-ggplot(perf) + 
-  geom_path(aes(x=TIME.M, y=SOG, color=pol.perc), size=1, lineend="round") +
-  ggtitle("GPS Speed Over Ground vs. Target")+
-  scale_color_gradientn(colors=rainbow(5), breaks=seq(0,120, by=20), name= "% of Target")+
-  scale_x_datetime(breaks=date_breaks("2 hours"), date_labels = "%a %H:%M")+
-  geom_path(aes(x=TIME.M, y=target.SOG, linetype="Target SOG"))+scale_linetype(name="Polars")
-
-
-
-
-
-#histogram of difference vs. Target
-ggplot(perf, aes(pol.perc)) + geom_histogram(col="black") + scale_x_continuous(breaks=seq(0,150, by=15))
+#convert Apparent to True
+getTrueWind <- function(AWS, AWA, SOG) { 
+  #outputs a list with 
+  # $tws = True Wind Speed
+  # $twa = True Wind Angle
   
+  b <- AWA * (pi/180)
+  
+  #true wind speed
+  TWS <- sqrt(AWS^2 + SOG^2 - 2*AWS*SOG*cos(b))
+  
+  
+  calcTWA <- function(b, AWS.b, SOG.b, W){
+    if(b > 0 & b<2*pi) {
+      
+      TWA <- acos(((AWS.b * cos(b)) - SOG.b)/W)
+      
+    } else {
+      if(b > 2*pi) {
+        TWA <- NA
+      }
+      
+      TWA <- -acos(AWS.b * cos(b) - SOG.b)
+      
+    }
+    
+    if(!is.na(TWA)){
+      
+      TWA <- TWA*180/pi
+      
+    }
+    return(TWA)
+  }
+  
+  TWA <- mapply(FUN=calcTWA, b, AWS, SOG, TWS)
+  
+  x <- list(TWS=TWS, TWA=TWA)
+  
+}
 
-pol_labels <- pol[pol$BTW==180, ]
-
-#difference vs. Polar
-ggplot() + 
-  geom_path(data=pol, aes(x=BAW, y=V, group=VTW)) + 
-  coord_polar("x") + 
-  scale_x_continuous(limits=c(0,360), breaks=seq(0,360, 15), name="Apparent Wind Angle AWA") + 
-  scale_y_continuous(name="SOG")+
-  geom_vline(xintercept=0)+
-  geom_vline(xintercept=90)+
-  geom_vline(xintercept=180)+
-  geom_vline(xintercept=270)+
-  ggtitle("% of Polar Performance by AWA")+
-  geom_point(data=perf, aes(x=AWA, y=SOG, color=pol.perc))+
-  scale_color_gradientn(colors=rainbow(5), breaks=seq(0,120, by=20), name= "% of Target")+
-  geom_text(data=pol_labels, aes(x=BTW, y=V, label=VTW), hjust=1)
+tw <- getTrueWind(perf$AWS, perf$AWA, perf$SOG)
+perf$TWS <- tw$TWS
+perf$TWA <- tw$TWA
 
 
-ggplot(perf, aes(x=TIME.M, y=SOG, color=pol.perc)) + geom_point() + 
-  scale_color_gradientn(colors=rainbow(5), breaks=seq(30, 120, 20))+
-  geom_path(aes(x=TIME.M, y=target.SOG), color="black")
+#break wind speed into bins: 
+perf$AWS.range <- cut(perf$AWS, breaks=c(0, 6, 8, 10, 12, 16, 20, 24, 35))
+perf$AWS.range <- factor(perf$AWS.range, levels=rev(levels(perf$AWS.range)))
+perf$TWS.range <- cut(perf$TWS, breaks=c(0, 6, 8, 10, 12, 16, 20, 24, 35))
+perf$TWS.range <- factor(perf$TWS.range, levels=rev(levels(perf$TWS.range)))
 
+#break wind angle into bins:
+perf$AWA.range <- cut(perf$AWA, breaks=seq(0,180, by=30))
+perf$TWA.range <- cut(perf$AWA, breaks=seq(0,180, by=30))
 
-ggplot(perf)+geom_histogram(aes(pol.perc), bins=30, color="black")+scale_x_continuous(breaks=seq(0, 150, by=10))+
-  geom_vline(col="red", xintercept=mean(perf$pol.perc, na.rm = TRUE))
-
-ggplot(perf)+geom_histogram(aes(pol.perc, fill=AWS.range), bins=30, color="black")+scale_x_continuous(breaks=seq(0, 150, by=10))+
-  geom_vline(col="red", xintercept=mean(perf$pol.perc, na.rm = TRUE))+facet_grid(AWS.range ~ .)
+write.csv("./dat/data_byminute.csv", row.names = FALSE)
